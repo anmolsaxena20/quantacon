6858dabcb35e9cd5681ecd3234e5f348fa2c41b5
GAURAV DUBEY <nikhlidubey200789@gmail.com>
2026-01-16 13:38:08 +0530

    setup frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='bg-amber-800 text-2xl'>Hii project</div>
+  )
+}
+
+export default App
