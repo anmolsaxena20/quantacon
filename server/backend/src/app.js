8e67a7f82b1fe04ac640c0e7b016471364722d8a
Anarok404 <anmolsxn2005@gmail.com>
2026-01-26 21:43:50 +0530

    Razorpay Integrated for tier upgradation

--- server/backend/src/app.js
+++ server/backend/src/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
-
+import authRoutes from "./routes/auth.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import { requireAuth } from "./middlewares/auth.middleware.js";
 const app = express();
 
 /* -------- Middlewares -------- */
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 /* -------- Routes -------- */
 app.use("/api/auth", authRoutes);
+app.use("/api/payment", paymentRoutes);
 
 /* -------- Health Check -------- */
 app.get("/", (req, res) => {
