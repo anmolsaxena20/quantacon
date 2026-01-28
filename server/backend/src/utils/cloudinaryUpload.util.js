bec0327dd7ca5e87c20e3c9856f64c59dc1f21e6
Anarok404 <anmolsxn2005@gmail.com>
2026-01-28 15:33:24 +0530

    reel upload bug resolved

--- server/backend/src/utils/cloudinaryUpload.util.js
+++ server/backend/src/utils/cloudinaryUpload.util.js
@@ -4,7 +4,7 @@ import streamifier from "streamifier";
 export const uploadToCloudinary = (fileBuffer, folder = "social_media") => {
   return new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
-      { folder },
+      { folder, resource_type: "auto" },
       (error, result) => {
         if (result) resolve(result);
         else reject(error);
