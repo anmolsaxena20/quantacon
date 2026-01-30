c5e94d8467a0f389e886736d38e7183c3bc5fe12
Anarok404 <anmolsxn2005@gmail.com>
2026-01-30 17:26:00 +0530

    minor bug fixes

--- server/backend/src/controllers/user.controller.js
+++ server/backend/src/controllers/user.controller.js
@@ -22,7 +22,6 @@ export const logout = async (req, res) => {
       sameSite: "lax",
       secure: false,
     });
-
     res.json({ message: "Logged out successfully" });
   } catch (err) {
     res.status(500).json({ message: "Logout failed" });
@@ -42,7 +41,10 @@ export const getUserDetails = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-
+    (await user.populate("followers", "name profilePic")).populate(
+      "following",
+      "name profilePic",
+    );
     res.status(200).json({ user });
   } catch (err) {
     console.error("Get user details error:", err.message);
